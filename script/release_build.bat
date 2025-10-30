1d291635bf7afb5813bd7582c3d37b76410f15f1
coolight <2465045051@qq.com>
2025-10-30 18:25:04 +0800

    add android ffmpeg include

--- script/release_build.bat
+++ script/release_build.bat
@@ -7,6 +7,6 @@ set src_dir=D:\0Acoolight\Program\flutter\mymusic\package\mediaxx\src
 
 cmake -B "%build_dir%" -S "%src_dir%" -DLUMENXX_BUILD_TYPE=LUMENXX_BUILD_RELEASE -DCMAKE_BUILD_TYPE=Release
 cmake --build "%build_dir%" --config release
-cmake --install "%build_dir%" --config Release
+cmake --install "%build_dir%" --config release
 
 %build_dir%\\exec\\test.exe
