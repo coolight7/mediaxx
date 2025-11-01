6ca118ac90082a1d05323e3d53671e8410a4fad5
coolight <2465045051@qq.com>
2025-11-01 19:47:24 +0800

    调整 log

--- src/lib/util/log.h
+++ src/lib/util/log.h
@@ -3,6 +3,8 @@
 #include <format>
 #include <iostream>
 
+#ifdef LUMENXX_BUILD_TYPE
+
 #define LXX_DEBEG(str, ...) (std::cerr << std::format(str, ##__VA_ARGS__) << std::endl);
 
 #define LXX_INFO(str, ...) (std::cerr << std::format(str, ##__VA_ARGS__) << std::endl);
@@ -10,3 +12,15 @@
 #define LXX_WARN(str, ...) (std::cerr << std::format(str, ##__VA_ARGS__) << std::endl);
 
 #define LXX_ERR(str, ...) (std::cerr << std::format(str, ##__VA_ARGS__) << std::endl);
+
+#else
+
+#define LXX_DEBEG(str, ...) ;
+
+#define LXX_INFO(str, ...) ;
+
+#define LXX_WARN(str, ...) ;
+
+#define LXX_ERR(str, ...) ;
+
+#endif
